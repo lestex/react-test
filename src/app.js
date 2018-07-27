d320897253bbd73229887b2ee4c28cf53bf825f3
Andrey Larin <lestex@gmail.com>
2018-07-27 15:42:33 +0300

    changes name for store

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/confugureStore';
+import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
