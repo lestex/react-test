53c09ad515c022c33ec73420416d11b57cdec366
Andrey Larin <lestex@gmail.com>
2018-07-30 21:59:09 +0300

    adds some changes

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,22 +5,22 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push('/');
   };
   render() {
     return (
       <div>
-      <ExpenseForm 
-        onSubmit={this.onSubmit}
-      />
-    </div>
-    )  
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   };
 }
 
-const mapDispatchToProps = (dispatch) => ({  
-  onAddExpense: (expense) => dispatch(addExpense(expense))
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
