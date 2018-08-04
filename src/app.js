668ddd5a457a41698728b3c5774143653fdf0ad4
Andrey Larin <lestex@gmail.com>
2018-08-04 18:39:51 +0300

    adds LoginPage and auth logic

--- src/app.js
+++ src/app.js
@@ -7,16 +7,25 @@ import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
 ReactDOM.render(<p>Loading</p>, document.getElementById('app'));
 store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log in');
+  } else {
+    console.log('log out');
+  }
 });
